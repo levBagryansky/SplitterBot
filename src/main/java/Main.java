21abd02505bd0cb10160b991c837e91254dc2b22
levBagryansky <28lev11@gmail.com>
2024-05-31 13:11:46 +0300

    removed token leak

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,12 @@ public class Main {
                 System.out.println(args[0]);
                 config = new BotConfig(args[0]);
             } else {
-                config = new BotConfig("7448458229:AAG5x5tvqSe_LJXHWhZBLpXa81JK3mRiFEM");
+                config = new ObjectMapper().readValue(new File(BotConfig.PATH), BotConfig.class);
             }
             //final DataBase dataBase = new NonSavableDataBase();
             new TelegramBotsApi(DefaultBotSession.class)
                 .registerBot(new Bot(config));
-        } catch (final TelegramApiException exc) {
+        } catch (final TelegramApiException | IOException exc) {
             exc.printStackTrace();
         }
     }
